46b33cd285402645a4f343c0aaef360ded9eef2c
Kalle Møller <git@k-moeller.dk>
2017-04-02 14:29:04 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,97 @@
 from conans import ConanFile, CMake, tools
 import os
+import re
+
+class Options:
+    def __init__(self, version, arch, exception, threads):
+        self.version = version
+        self.arch = arch
+        self.exception = exception
+        self.threads = threads
+
+def create_releases(version, archs, exceptions, threads, sub_version, rt, revision):
+    for arch in archs:
+        for exception in exceptions:
+            for thread in threads:
+                if (arch == "x86" and exception == "seh"):
+                    continue
+                if (arch == "x86_64" and exception == "dwarf2"):
+                    continue
+                yield MinGWRelease(Options(version, arch, exception, thread), sub_version, rt, revision)
+
+class MinGWCollection:
+    def __init__(self):
+        self.collection = {}
+        self.threads = set()
+        self.exception = set()
+        self.arch = set()
+        self.version = set()
+        
+    def generate_key(self, options):
+        return (options.version, options.arch, options.exception, options.threads)
+
+    def add_options(self, options):
+        self.threads.add(options.threads)
+        self.exception.add(options.exception)
+        self.arch.add(options.arch)
+        self.version.add(options.version)
+    
+    def add(self, release):
+        key = self.generate_key(release.options)
+        if key in self.collection:
+            raise Exception()
+        
+        self.add_options(release.options)
+        
+        self.collection[key] = release
+    
+    def add_list(self, releases):
+        for release in releases:
+            self.add(release)
+
+    def get_options(self):
+        return {
+            "threads": list(self.threads),
+            "exception": list(self.exception),
+            "arch": list(self.arch),
+            "version": list(self.version),
+        }
+    def get_url(self, options):
+        key = self.generate_key(options)
+        return self.collection[key].url
+    
+    def validate_options(self, options):
+        key = self.generate_key(options)
+        if key not in self.collection:
+            raise Exception("Not valid %s and %s combination!" % (options.arch, options.exception))   
+
+    def __str__(self):
+        return str(self.collection)
+        
+
+class MinGWRelease:
+
+    def __init__(self, options, sub_version, rt, revision):
+        self.options = options
+        self.sub_version = sub_version
+        self.rt = rt
+        self.revision = revision
+    
+    @property
+    def url(self):
+        version = self.options.version + ('.%s' % self.sub_version if self.sub_version else '')
+        arch = self.options.arch
+        threads = self.options.threads
+        exception = self.options.exception
+        rt = self.rt
+        revision = self.revision
+        
+        url = 'http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds'
+        path = '%s/threads-%s/%s' % (version, threads, exception)
+        file = '%s-%s-release-%s-%s-rt_v%s-rev%s.7z' % (arch, version, threads, exception, rt, revision)
+        
+        return url + '/' + path + '/' + file
+
 
 
 class MingwinstallerConan(ConanFile):
@@ -8,63 +100,23 @@ class MingwinstallerConan(ConanFile):
     license = "MIT"
     url = "http://github.com/lasote/conan-mingw-installer"
     settings = {"os": ["Windows"]}
-    options = {"threads": ["posix", "win32"],
-               "exception": ["dwarf2", "sjlj", "seh"], 
-               "arch": ["x86", "x86_64"],
-               "version": ["4.8", "4.9", "5.4", "6.2"]}
+    
+    mingw_collection = MinGWCollection()
+    mingw_collection.add_list(create_releases("4.8", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "2", "3", "0"))
+    mingw_collection.add_list(create_releases("4.9", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "2", "3", "1"))
+    mingw_collection.add_list(create_releases("5.4", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "0"))
+    mingw_collection.add_list(create_releases("6.2", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "1"))
+    options = mingw_collection.get_options()
+    
     default_options = "exception=sjlj", "threads=posix", "arch=x86_64", "version=4.9"
     build_policy = "missing"
 
-
     def configure(self):
         self.requires.add("7z_installer/0.1@lasote/testing", private=True)
-        if (self.options.arch == "x86" and self.options.exception == "seh") or \
-           (self.options.arch == "x86_64" and self.options.exception == "dwarf2"):
-            raise Exception("Not valid %s and %s combination!" % (self.options.arch, 
-                                                                  self.options.exception))       
+        self.mingw_collection.validate_options(self.options)
+
     def build(self):
-        keychain = "%s_%s_%s_%s" % (str(self.options.version).replace(".", ""), 
-                                    self.options.arch,
-                                    self.options.exception,
-                                    self.options.threads)
-        files = {"48_x86_dwarf2_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.8.2/threads-posix/dwarf/i686-4.8.2-release-posix-dwarf-rt_v3-rev0.7z",
-                 "48_x86_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.8.2/threads-posix/sjlj/i686-4.8.2-release-posix-sjlj-rt_v3-rev0.7z",
-                 "48_x86_64_seh_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.8.2/threads-posix/seh/x86_64-4.8.2-release-posix-seh-rt_v3-rev0.7z",
-                 "48_x86_64_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.8.2/threads-posix/sjlj/x86_64-4.8.2-release-posix-sjlj-rt_v3-rev0.7z",
-                 "48_x86_dwarf2_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.8.2/threads-win32/dwarf/i686-4.8.2-release-win32-dwarf-rt_v3-rev0.7z",
-                 "48_x86_sjlj_win32": "https://github.com/tim-lebedkov/packages/releases/download/2016_Q2/i686-4.9.2-release-win32-sjlj-rt_v3-rev1.7z",
-                 "48_x86_64_seh_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.8.2/threads-win32/seh/x86_64-4.8.2-release-win32-seh-rt_v3-rev0.7z",
-                 "48_x86_64_sjlj_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.8.2/threads-win32/sjlj/x86_64-4.8.2-release-win32-sjlj-rt_v3-rev0.7z",
-                 "49_x86_dwarf2_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.2/threads-posix/dwarf/i686-4.9.2-release-posix-dwarf-rt_v3-rev1.7z",
-                 "49_x86_sjlj_posix": "https://github.com/tim-lebedkov/packages/releases/download/2016_Q1/i686-4.9.2-release-posix-sjlj-rt_v3-rev0.7z",
-                 "49_x86_64_seh_posix": "https://github.com/tim-lebedkov/packages/releases/download/2016_Q1/x86_64-4.9.2-release-posix-seh-rt_v3-rev1.7z",
-                 "49_x86_64_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-posix/sjlj/x86_64-4.9.2-release-posix-sjlj-rt_v3-rev1.7z",
-                 "49_x86_dwarf2_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.2/threads-win32/dwarf/i686-4.9.2-release-win32-dwarf-rt_v3-rev1.7z",
-                 "49_x86_sjlj_win32": "https://github.com/tim-lebedkov/packages/releases/download/2016_Q2/i686-4.9.2-release-win32-sjlj-rt_v3-rev1.7z",
-                 "49_x86_64_seh_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-win32/seh/x86_64-4.9.2-release-win32-seh-rt_v3-rev1.7z",
-                 "49_x86_64_sjlj_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-win32/sjlj/x86_64-4.9.2-release-win32-sjlj-rt_v3-rev1.7z",
-
-                 "62_x86_dwarf_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-win32/dwarf/i686-6.2.0-release-win32-dwarf-rt_v5-rev1.7z",
-                 "62_x86_sjlj_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-win32/sjlj/i686-6.2.0-release-win32-sjlj-rt_v5-rev1.7z",
-                 "62_x86_dwarf_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-posix/dwarf/i686-6.2.0-release-posix-dwarf-rt_v5-rev1.7z",
-                 "62_x86_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-posix/sjlj/i686-6.2.0-release-posix-sjlj-rt_v5-rev1.7z",
-
-                 "62_x86_64_seh_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-win32/seh/x86_64-6.2.0-release-win32-seh-rt_v5-rev1.7z",
-                 "62_x86_64_sjlj_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-win32/sjlj/x86_64-6.2.0-release-win32-sjlj-rt_v5-rev1.7z",
-                 "62_x86_64_seh_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-posix/seh/x86_64-6.2.0-release-posix-seh-rt_v5-rev1.7z",
-                 "62_x86_64_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/6.2.0/threads-posix/sjlj/x86_64-6.2.0-release-posix-sjlj-rt_v5-rev1.7z",
-                                        
-                 "54_x86_dwarf_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.4.0/threads-win32/dwarf/i686-5.4.0-release-win32-dwarf-rt_v5-rev0.7z",
-                 "54_x86_sjlj_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.4.0/threads-win32/sjlj/i686-5.4.0-release-win32-sjlj-rt_v5-rev0.7z",
-                 "54_x86_dwarf_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.4.0/threads-posix/dwarf/i686-5.4.0-release-posix-dwarf-rt_v5-rev0.7z",
-                 "54_x86_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/5.4.0/threads-posix/sjlj/i686-5.4.0-release-posix-sjlj-rt_v5-rev0.7z",
-
-                 "54_x86_64_seh_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.4.0/threads-win32/seh/x86_64-5.4.0-release-win32-seh-rt_v5-rev0.7z",
-                 "54_x86_64_sjlj_win32": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.4.0/threads-win32/sjlj/x86_64-5.4.0-release-win32-sjlj-rt_v5-rev0.7z",
-                 "54_x86_64_seh_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.4.0/threads-posix/seh/x86_64-5.4.0-release-posix-seh-rt_v5-rev0.7z",
-                 "54_x86_64_sjlj_posix": "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.4.0/threads-posix/sjlj/x86_64-5.4.0-release-posix-sjlj-rt_v5-rev0.7z",}
-
-        tools.download(files[keychain], "file.7z")
+        tools.download(self.mingw_collection.get_url(self.options), "file.7z")
         self.run("7z x file.7z")
     
     def package(self):
