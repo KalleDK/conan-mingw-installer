ee08e6ae15d67e850ec6dc20be484e2a616c7c25
Kalle R. Møller <git@k-moeller.dk>
2017-04-02 15:39:55 +0200

    Generic releases

--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,27 @@
 from conans import ConanFile, CMake, tools
 import os
 import re
+import urllib
 
-class Options:
+class SimpleOptions:
     def __init__(self, version, arch, exception, threads):
         self.version = version
         self.arch = arch
         self.exception = exception
         self.threads = threads
 
-def create_releases(version, archs, exceptions, threads, sub_version, rt, revision):
-    for arch in archs:
-        for exception in exceptions:
-            for thread in threads:
-                if (arch == "x86" and exception == "seh"):
-                    continue
-                if (arch == "x86_64" and exception == "dwarf2"):
-                    continue
-                yield MinGWRelease(Options(version, arch, exception, thread), sub_version, rt, revision)
-
 class MinGWCollection:
-    def __init__(self):
+    def __init__(self, release_matrixes):
         self.collection = {}
         self.threads = set()
         self.exception = set()
         self.arch = set()
         self.version = set()
+        for release_matrix in release_matrixes:
+            self.add_release_matrix(*release_matrix)
         
     def generate_key(self, options):
-        return (options.version, options.arch, options.exception, options.threads)
+        return (str(options.version), str(options.arch), str(options.exception), str(options.threads))
 
     def add_options(self, options):
         self.threads.add(options.threads)
@@ -39,23 +32,30 @@ class MinGWCollection:
     def add(self, release):
         key = self.generate_key(release.options)
         if key in self.collection:
-            raise Exception()
+            raise Exception("Duplicate package added %s" % release)
         
         self.add_options(release.options)
         
         self.collection[key] = release
     
-    def add_list(self, releases):
-        for release in releases:
-            self.add(release)
+    def add_release_matrix(self, version, archs, exceptions, threads, sub_version, rt, revision):
+        for arch in archs:
+            for exception in exceptions:
+                for thread in threads:
+                    if (arch == "x86" and exception == "seh"):
+                        continue
+                    if (arch == "x86_64" and exception == "dwarf2"):
+                        continue
+                    self.add(MinGWRelease(SimpleOptions(version, arch, exception, thread), sub_version, rt, revision))
 
-    def get_options(self):
+    def generate_options(self):
         return {
             "threads": list(self.threads),
             "exception": list(self.exception),
             "arch": list(self.arch),
             "version": list(self.version),
         }
+
     def get_url(self, options):
         key = self.generate_key(options)
         return self.collection[key].url
@@ -63,12 +63,8 @@ class MinGWCollection:
     def validate_options(self, options):
         key = self.generate_key(options)
         if key not in self.collection:
-            raise Exception("Not valid %s and %s combination!" % (options.arch, options.exception))   
-
-    def __str__(self):
-        return str(self.collection)
+            raise Exception("Not valid %s and %s combination!" % (options.arch, options.exception))
         
-
 class MinGWRelease:
 
     def __init__(self, options, sub_version, rt, revision):
@@ -80,35 +76,37 @@ class MinGWRelease:
     @property
     def url(self):
         version = self.options.version + ('.%s' % self.sub_version if self.sub_version else '')
-        arch = self.options.arch
+        arch = self.options.arch if self.options.arch is not "x86" else "i686"
+        path_arch = 'Win64' if arch == "x86_64" else 'Win32'
         threads = self.options.threads
-        exception = self.options.exception
+        exception = self.options.exception if self.options.exception is not "dwarf2" else "dwarf"
         rt = self.rt
         revision = self.revision
-        
-        url = 'http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds'
-        path = '%s/threads-%s/%s' % (version, threads, exception)
+        url = 'downloads.sourceforge.net/project/mingw-w64'
+        path = 'Toolchains targetting %s/Personal Builds/mingw-builds/%s/threads-%s/%s' % (path_arch, version, threads, exception)
         file = '%s-%s-release-%s-%s-rt_v%s-rev%s.7z' % (arch, version, threads, exception, rt, revision)
         
-        return url + '/' + path + '/' + file
+        return 'http://' + urllib.parse.quote(url + '/' + path + '/' + file)
 
-
+mingw_matrix = MinGWCollection((
+    ("4.8", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "2", "3", "0"),
+    ("4.9", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "2", "3", "1"),
+    ("5.4", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "0"),
+    ("6.2", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "1"),
+    ("6.3", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "1"),
+))
 
 class MingwinstallerConan(ConanFile):
     name = "mingw_installer"
     version = "0.1"
     license = "MIT"
-    url = "http://github.com/lasote/conan-mingw-installer"
+    url = "http://github.com/KalleDK/conan-mingw-installer"
     settings = {"os": ["Windows"]}
     
-    mingw_collection = MinGWCollection()
-    mingw_collection.add_list(create_releases("4.8", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "2", "3", "0"))
-    mingw_collection.add_list(create_releases("4.9", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "2", "3", "1"))
-    mingw_collection.add_list(create_releases("5.4", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "0"))
-    mingw_collection.add_list(create_releases("6.2", ["x86", "x86_64"], ["sjlj", "seh", "dwarf2"], ["posix", "win32"], "0", "5", "1"))
-    options = mingw_collection.get_options()
+    mingw_collection = mingw_matrix
+    options = mingw_collection.generate_options()
     
-    default_options = "exception=sjlj", "threads=posix", "arch=x86_64", "version=4.9"
+    default_options = "exception=sjlj", "threads=posix", "arch=x86_64", "version=6.3"
     build_policy = "missing"
 
     def configure(self):
